4f44822f13c69d750315731ad8962e2f9e7337d7
Ryan Bradford <ryan_bradford@msn.com>
2023-05-30 18:57:41 -0400

    Document.write and "for" loop addition
    Added a "for" loop that commented on the pokemon's size depending on if it was small, average, or large; this is after printing the name, height, and type of the pokemon. Also tidied up the code to make it more readable.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,175 @@
 let pokemonList = [
-    {name : "bulbasaur", height : 0.7, type : ["grass", "poison"]},
-    {name : "ivysaur", height : 1, type : ["grass", "poison"]},
-    {name : "venusaur", height : 2, type : ["grass", "poison"]},
-    {name : "charmander", height : 0.6, type : "fire"},
-    {name : "charmeleon", height : 1.1, type : "fire"},
-    {name : "charizard", height : 1.7, type : ["fire", "flying"]},
-    {name : "squirtle", height : 0.5, type : "water"},
-    {name : "wartortle", height : 0.1, type : "water"},
-    {name : "blastoise", height : 1.6, type : "water"},
-    {name : "caterpie", height : 0.3, type : "bug"},
-    {name : "metapod", height : 0.7, type : "bug"},
-    {name : "butterfree", height : 1.1, type : ["bug", "flying"]},
-    {name : "weedle", height : 0.3, type : ["bug", "poison"]},
-    {name : "kakuna", height : 0.6, type : ["bug", "poison"]},
-    {name : "beedrill", height : 1, type : ["bug", "poison"]},
-    {name : "pidgey", height : 0.3, type : ["flying", "normal"]},
-    {name : "pidgeotto", height : 1.1, type : ["flying", "normal"]},
-    {name : "pidgeot", height : 1.5, type : ["flying", "normal"]},
-    {name : "rattata", height : 0.3, type : "normal"},
-    {name : "raticate", height : 0.7, type : "normal"},
-    {name : "spearow", height : 0.3, type : "normal"},
-    {name : "fearow", height : 1.2, type : ["flying", "normal"]},
-    {name : "ekans", height : 2, type : "poison"},
-    {name : "arbok", height : 3.5, type : "poison"},
-    {name : "pikachu", height : 0.4, type : "electric"},
-    {name : "raichu", height : 0.8, type : "electric"},
-    {name : "sandshrew", height : 0.6, type : "ground"},
-    {name : "sandslash", height : 1, type : "ground"},
-    {name : "nidoran", height : 0.4, type : "poison"},
-    {name : "nidorina", height : 0.8, type : "poison"},
-
+    //lists pokemon and their height (in meters) and type
+    {
+        name : "Bulbasaur", 
+        height : 0.7, 
+        type : ["grass", "poison"]
+    },
+    {
+        name : "Ivysaur", 
+        height : 1, 
+        type : ["grass", "poison"]
+    },
+    {
+        name : "Venusaur", 
+        height : 2, 
+        type : ["grass", "poison"]
+    },
+    {
+        name : "Charmander", 
+        height : 0.6, 
+        type : "fire"
+    },
+    {
+        name : "Charmeleon", 
+        height : 1.1, 
+        type : "fire"
+    },
+    {
+        name : "Charizard", 
+        height : 1.7, 
+        type : ["fire", "flying"]
+    },
+    {
+        name : "Squirtle", 
+        height : 0.5, 
+        type : "water"
+    },
+    {
+        name : "Wartortle", 
+        height : 0.1, 
+        type : "water"
+    },
+    {
+        name : "Blastoise", 
+        height : 1.6, 
+        type : "water"
+    },
+    {
+        name : "Caterpie", 
+        height : 0.3, 
+        type : "bug"
+    },
+    {
+        name : "Metapod", 
+        height : 0.7, 
+        type : "bug"
+    },
+    {
+        name : "Butterfree", 
+        height : 1.1, 
+        type : ["bug", "flying"]
+    },
+    {
+        name : "Weedle", 
+        height : 0.3, 
+        type : ["bug", "poison"]
+    },
+    {
+        name : "Kakuna", 
+        height : 0.6, 
+        type : ["bug", "poison"]
+    },
+    {
+        name : "Beedrill", 
+        height : 1, 
+        type : ["bug", "poison"]
+    },
+    {
+        name : "Pidgey", 
+        height : 0.3, 
+        type : ["flying", "normal"]
+    },
+    {
+        name : "Pidgeotto", 
+        height : 1.1, 
+        type : ["flying", "normal"]
+    },
+    {
+        name : "Pidgeot", 
+        height : 1.5, 
+        type : ["flying", "normal"]
+    },
+    {
+        name : "Rattata", 
+        height : 0.3, 
+        type : "normal"
+    },
+    {
+        name : "Raticate", 
+        height : 0.7, 
+        type : "normal"
+    },
+    {
+        name : "Spearow", 
+        height : 0.3, 
+        type : "normal"
+    },
+    {
+        name : "Fearow", 
+        height : 1.2, 
+        type : ["flying", "normal"]
+    },
+    {
+        name : "Ekans", 
+        height : 2, 
+        type : "poison"
+    },
+    {
+        name : "Arbok", 
+        height : 3.5, 
+        type : "poison"
+    },
+    {
+        name : "Pikachu", 
+        height : 0.4, 
+        type : "electric"
+    },
+    {
+        name : "Raichu", 
+        height : 0.8, 
+        type : "electric"
+    },
+    {
+        name : "Sandshrew",
+        height : 0.6, 
+        type : "ground"
+    },
+    {
+        name : "Sandslash", 
+        height : 1, 
+        type : "ground"
+    },
+    {
+        name : "Nidoran", 
+        height : 0.4, 
+        type : "poison"
+    },
+    {
+        name : "Nidorina", 
+        height : 0.8, 
+        type : "poison"
+    },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(
+        "Name:" + " " + pokemonList[i].name + '<br>' + "Height (m):" + " " + pokemonList[i].height + '<br>' + "Type:" + " " + pokemonList[i].type + '<br>'
+        //prints pokemon's name, height, and type(s)
+    )
+
+
+    if (pokemonList[i].height < 1.5 && pokemonList[i].height >= 0.5){
+        document.write (
+            pokemonList[i].name + " is an average sized pokemon" + '<br><br>'); 
+            //if pokemon is equal to or larger than .5m and smaller than 1.5m, it will return as "is an average sized pokemon"
+    
+        }else if (pokemonList[i].height >= 1.5){
+            document.write (pokemonList[i].name + " is a large sized pokemon" + '<br><br>');
+            //if pokemon is larger or equal to 1.5m, it will return as "is a large sized pokemon"
+
+        }else {
+            document.write (pokemonList[i].name + " is a small sized pokemon" + '<br><br>');
+            //if pokemon is smaller than 0.5m, it will return as "is a small sized pokemon"
+    }
+}
