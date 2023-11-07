c3e99e69ec82b9d7c5bce8f69a30def452f0df02
Ryan Bradford <ryan_bradford@msn.com>
2023-11-07 16:45:11 -0500

    Replaced pokemon list with Pokemon API source
    Added polyfills, sourced a pokemon repository from pokeapi, and set a GET (fetch) function to return a JSON response.

--- js/script.js
+++ js/script.js
@@ -1,165 +1,12 @@
 let pokemonRepository = (function () {
-  
-    let pokemonList = [
-        //lists pokemon and their height (in meters) and type
-        {
-            name : "Bulbasaur", 
-            height : 0.7, 
-            type : ["grass", "poison"]
-        },
-        {
-            name : "Ivysaur", 
-            height : 1, 
-            type : ["grass", "poison"]
-        },
-        {
-            name : "Venusaur", 
-            height : 2, 
-            type : ["grass", "poison"]
-        },
-        {
-            name : "Charmander", 
-            height : 0.6, 
-            type : "fire"
-        },
-        {
-            name : "Charmeleon", 
-            height : 1.1, 
-            type : "fire"
-        },
-        {
-            name : "Charizard", 
-            height : 1.7, 
-            type : ["fire", "flying"]
-        },
-        {
-            name : "Squirtle", 
-            height : 0.5, 
-            type : "water"
-        },
-        {
-            name : "Wartortle", 
-            height : 0.1, 
-            type : "water"
-        },
-        {
-            name : "Blastoise", 
-            height : 1.6, 
-            type : "water"
-        },
-        {
-            name : "Caterpie", 
-            height : 0.3, 
-            type : "bug"
-        },
-        {
-            name : "Metapod", 
-            height : 0.7, 
-            type : "bug"
-        },
-        {
-            name : "Butterfree", 
-            height : 1.1, 
-            type : ["bug", "flying"]
-        },
-        {
-            name : "Weedle", 
-            height : 0.3, 
-            type : ["bug", "poison"]
-        },
-        {
-            name : "Kakuna", 
-            height : 0.6, 
-            type : ["bug", "poison"]
-        },
-        {
-            name : "Beedrill", 
-            height : 1, 
-            type : ["bug", "poison"]
-        },
-        {
-            name : "Pidgey", 
-            height : 0.3, 
-            type : ["flying", "normal"]
-        },
-        {
-            name : "Pidgeotto", 
-            height : 1.1, 
-            type : ["flying", "normal"]
-        },
-        {
-            name : "Pidgeot", 
-            height : 1.5, 
-            type : ["flying", "normal"]
-        },
-        {
-            name : "Rattata", 
-            height : 0.3, 
-            type : "normal"
-        },
-        {
-            name : "Raticate", 
-            height : 0.7, 
-            type : "normal"
-        },
-        {
-            name : "Spearow", 
-            height : 0.3, 
-            type : "normal"
-        },
-        {
-            name : "Fearow", 
-            height : 1.2, 
-            type : ["flying", "normal"]
-        },
-        {
-            name : "Ekans", 
-            height : 2, 
-            type : "poison"
-        },
-        {
-            name : "Arbok", 
-            height : 3.5, 
-            type : "poison"
-        },
-        {
-            name : "Pikachu", 
-            height : 0.4, 
-            type : "electric"
-        },
-        {
-            name : "Raichu", 
-            height : 0.8, 
-            type : "electric"
-        },
-        {
-            name : "Sandshrew",
-            height : 0.6, 
-            type : "ground"
-        },
-        {
-            name : "Sandslash", 
-            height : 1, 
-            type : "ground"
-        },
-        {
-            name : "Nidoran", 
-            height : 0.4, 
-            type : "poison"
-        },
-        {
-            name : "Nidorina", 
-            height : 0.8, 
-            type : "poison"
-        }
-    ]
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
     function add(pokemon) {
         if (
             typeof pokemon === "object" &&
             "name" in pokemon &&
-            "height" in pokemon &&
-            "type" in pokemon
+            "detailsUrl" in pokemon
           ) {
             pokemonList.push(pokemon);
           } else {
@@ -184,20 +31,57 @@ let pokemonRepository = (function () {
         pokemonList.appendChild(listPokemon);
     }
 
-    function showDetails(pokemon){
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
             console.log(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
+        })
+    }
+    
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+    }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+          console.log(pokemon);
+        });
     }
     
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
-        showDetails: showDetails
+        showDetails: showDetails,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
   })();
   
 console.log(pokemonRepository.getAll());
   
-pokemonRepository.getAll().forEach(function (pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function (pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
 });
